d843830913a8e67eb524420cc90c052f881e3cb5
Your Name <josuess13@gmail.com>
2024-07-03 13:52:20 -0300

    novos recrutas, correção trigger cria_guerreiros_apos_cadastro

--- DB/SQLs/tabelas.sql
+++ DB/SQLs/tabelas.sql
@@ -6,7 +6,7 @@ CREATE TABLE "jogadores" (  -- 1
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
-CREATE TABLE "imagem_recruta" ( 
+CREATE TABLE "imagem_recruta_masculinos" ( 
 	"id"	INTEGER NOT NULL,
 	"caminho_imagem"	TEXT NOT NULL,
 	PRIMARY KEY("id")
@@ -14,30 +14,47 @@ CREATE TABLE "imagem_recruta" (
  
 -- Fazer os inserts para as imagens
 
-CREATE TABLE "imagem_guerreiros" ( 
+CREATE TABLE "imagem_guerreiros_masculinos" ( 
 	"id"	INTEGER NOT NULL,
 	"caminho_imagem"	TEXT NOT NULL,
 	"nivel"	INTEGER NOT NULL,
 	PRIMARY KEY("id")
 );
 
+CREATE TABLE "imagem_recruta_femininos" ( 
+	"id"	INTEGER NOT NULL,
+	"caminho_imagem"	TEXT NOT NULL,
+	PRIMARY KEY("id")
+);
+ 
 -- Fazer os inserts para as imagens
 
-CREATE TABLE "recrutas" ( -- 2
+CREATE TABLE "imagem_guerreiros_femininos" ( 
+	"id"	INTEGER NOT NULL,
+	"caminho_imagem"	TEXT NOT NULL,
+	"nivel"	INTEGER NOT NULL,
+	PRIMARY KEY("id")
+);
+-- Fazer os inserts para as imagens
+
+CREATE TABLE "recrutas" (
 	"id"	INTEGER NOT NULL,
 	"id_jogador"	INTEGER NOT NULL,
 	"nome"	TEXT NOT NULL,
 	"ataque"	REAL NOT NULL,
 	"defesa"	REAL NOT NULL,
 	"inteligencia"	REAL NOT NULL,
-	"forca"	REAL NOT NULL,
-	"agilidade"	REAL NOT NULL,
+	"forca"	NUMERIC NOT NULL,
+	"agilidade"	NUMERIC NOT NULL,
 	"idade"	INTEGER NOT NULL,
-	"imagem_recruta_id"	TEXT NOT NULL,
-	"status_recruta"	INTEGER NOT NULL, -- 0 Disponível, 1 Recrutado
-	PRIMARY KEY("id" AUTOINCREMENT),
-	FOREIGN KEY("id_jogador") REFERENCES "jogadores"("id")
-	FOREIGN KEY("imagem_recruta_id") REFERENCES "imagem_recruta"("id")
+	"imagem_recruta_masculinos"	INTEGER,
+	"imagem_recruta_femininos"	INTEGER,
+	"status_recruta"	INTEGER NOT NULL,
+	"sexo"	TEXT NOT NULL,
+	FOREIGN KEY("imagem_recruta_masculinos") REFERENCES "imagem_recruta_masculinos"("id"),
+	FOREIGN KEY("imagem_recruta_femininos") REFERENCES "imagem_recruta_femininos"("id"),
+	FOREIGN KEY("id_jogador") REFERENCES "jogadores"("id"),
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
 CREATE TABLE "guerreiros" (
@@ -53,165 +70,22 @@ CREATE TABLE "guerreiros" (
 	"imagem_guerreiro_id"	TEXT NOT NULL,
     "vitorias"	INTEGER NOT NULL,
 	"nivel"	INTEGER NOT NULL,
+	"sexo"	TEXT NOT NULL, -- M Masculino, F Feminino
 	PRIMARY KEY("id" AUTOINCREMENT),
 	FOREIGN KEY("id_jogador") REFERENCES "jogadores"("id")
 );
 
-CREATE TABLE "nomes" (
+CREATE TABLE "nomes_masculinos" (
 	"id"	INTEGER NOT NULL,
 	"nome"	TEXT NOT NULL,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
-INSERT INTO nomes(nome) VALUES('Achilles');
-INSERT INTO nomes(nome) VALUES('Adonis');
-INSERT INTO nomes(nome) VALUES('Ajax');
-INSERT INTO nomes(nome) VALUES('Anastasios');
-INSERT INTO nomes(nome) VALUES('Andreas');
-INSERT INTO nomes(nome) VALUES('Antonios');
-INSERT INTO nomes(nome) VALUES('Apollo');
-INSERT INTO nomes(nome) VALUES('Argus');
-INSERT INTO nomes(nome) VALUES('Aristotle');
-INSERT INTO nomes(nome) VALUES('Atlas');
-INSERT INTO nomes(nome) VALUES('Basil');
-INSERT INTO nomes(nome) VALUES('Cassander');
-INSERT INTO nomes(nome) VALUES('Damon');
-INSERT INTO nomes(nome) VALUES('Dimitrios');
-INSERT INTO nomes(nome) VALUES('Eros');
-INSERT INTO nomes(nome) VALUES('Evander');
-INSERT INTO nomes(nome) VALUES('Hector');
-INSERT INTO nomes(nome) VALUES('Hermes');
-INSERT INTO nomes(nome) VALUES('Icarus');
-INSERT INTO nomes(nome) VALUES('Kyros');
-INSERT INTO nomes(nome) VALUES('Leander');
-INSERT INTO nomes(nome) VALUES('Leonidas');
-INSERT INTO nomes(nome) VALUES('Midas');
-INSERT INTO nomes(nome) VALUES('Nestor');
-INSERT INTO nomes(nome) VALUES('Odysseus');
-INSERT INTO nomes(nome) VALUES('Orpheus');
-INSERT INTO nomes(nome) VALUES('Pallas');
-INSERT INTO nomes(nome) VALUES('Paris');
-INSERT INTO nomes(nome) VALUES('Perseus');
-INSERT INTO nomes(nome) VALUES('Phoenix');
-INSERT INTO nomes(nome) VALUES('Plato');
-INSERT INTO nomes(nome) VALUES('Pyrrhus');
-INSERT INTO nomes(nome) VALUES('Rhodes');
-INSERT INTO nomes(nome) VALUES('Socrates');
-INSERT INTO nomes(nome) VALUES('Solon');
-INSERT INTO nomes(nome) VALUES('Thaddeus');
-INSERT INTO nomes(nome) VALUES('Theo');
-INSERT INTO nomes(nome) VALUES('Titan');
-INSERT INTO nomes(nome) VALUES('Xander');
-INSERT INTO nomes(nome) VALUES('Xenon');
-INSERT INTO nomes(nome) VALUES('Zenon');
-INSERT INTO nomes(nome) VALUES('Alexios');
-INSERT INTO nomes(nome) VALUES('Cleon');
-INSERT INTO nomes(nome) VALUES('Damian');
-INSERT INTO nomes(nome) VALUES('Evangelos');
-INSERT INTO nomes(nome) VALUES('Gregorios');
-INSERT INTO nomes(nome) VALUES('Ilias');
-INSERT INTO nomes(nome) VALUES('Lucian');
-INSERT INTO nomes(nome) VALUES('Marios');
-INSERT INTO nomes(nome) VALUES('Nikos');
-INSERT INTO nomes(nome) VALUES('Agamemnon');
-INSERT INTO nomes(nome) VALUES('Alcibiades');
-INSERT INTO nomes(nome) VALUES('Anaxagoras');
-INSERT INTO nomes(nome) VALUES('Antigonus');
-INSERT INTO nomes(nome) VALUES('Archelaus');
-INSERT INTO nomes(nome) VALUES('Aristarchus');
-INSERT INTO nomes(nome) VALUES('Aristides');
-INSERT INTO nomes(nome) VALUES('Aristophanes');
-INSERT INTO nomes(nome) VALUES('Artaxerxes');
-INSERT INTO nomes(nome) VALUES('Astyages');
-INSERT INTO nomes(nome) VALUES('Bardiya');
-INSERT INTO nomes(nome) VALUES('Cambyses');
-INSERT INTO nomes(nome) VALUES('Cyrus');
-INSERT INTO nomes(nome) VALUES('Darius');
-INSERT INTO nomes(nome) VALUES('Datis');
-INSERT INTO nomes(nome) VALUES('Demetrius');
-INSERT INTO nomes(nome) VALUES('Ephialtes');
-INSERT INTO nomes(nome) VALUES('Epictetus');
-INSERT INTO nomes(nome) VALUES('Euripides');
-INSERT INTO nomes(nome) VALUES('Gordias');
-INSERT INTO nomes(nome) VALUES('Heraclitus');
-INSERT INTO nomes(nome) VALUES('Herodotus');
-INSERT INTO nomes(nome) VALUES('Hippocrates');
-INSERT INTO nomes(nome) VALUES('Isocrates');
-INSERT INTO nomes(nome) VALUES('Leonidas');
-INSERT INTO nomes(nome) VALUES('Lycurgus');
-INSERT INTO nomes(nome) VALUES('Miltiades');
-INSERT INTO nomes(nome) VALUES('Minos');
-INSERT INTO nomes(nome) VALUES('Nicias');
-INSERT INTO nomes(nome) VALUES('Parmenides');
-INSERT INTO nomes(nome) VALUES('Pausanias');
-INSERT INTO nomes(nome) VALUES('Pericles');
-INSERT INTO nomes(nome) VALUES('Phidias');
-INSERT INTO nomes(nome) VALUES('Ptolemy');
-INSERT INTO nomes(nome) VALUES('Pythagoras');
-INSERT INTO nomes(nome) VALUES('Socrates');
-INSERT INTO nomes(nome) VALUES('Solon');
-INSERT INTO nomes(nome) VALUES('Sophonias');
-INSERT INTO nomes(nome) VALUES('Spartacus');
-INSERT INTO nomes(nome) VALUES('Strabo');
-INSERT INTO nomes(nome) VALUES('Themistocles');
-INSERT INTO nomes(nome) VALUES('Theocritus');
-INSERT INTO nomes(nome) VALUES('Thucydides');
-INSERT INTO nomes(nome) VALUES('Timon');
-INSERT INTO nomes(nome) VALUES('Xerxes');
-INSERT INTO nomes(nome) VALUES('Xanthippus');
-INSERT INTO nomes(nome) VALUES('Zeno');
-INSERT INTO nomes(nome) VALUES('Abel');
-INSERT INTO nomes(nome) VALUES('Arash');
-INSERT INTO nomes(nome) VALUES('Ardashir');
-INSERT INTO nomes(nome) VALUES('Arezou');
-INSERT INTO nomes(nome) VALUES('Aria');
-INSERT INTO nomes(nome) VALUES('Bahram');
-INSERT INTO nomes(nome) VALUES('Barbad');
-INSERT INTO nomes(nome) VALUES('Behzad');
-INSERT INTO nomes(nome) VALUES('Bijan');
-INSERT INTO nomes(nome) VALUES('Dariush');
-INSERT INTO nomes(nome) VALUES('Darab');
-INSERT INTO nomes(nome) VALUES('Farhad');
-INSERT INTO nomes(nome) VALUES('Farshad');
-INSERT INTO nomes(nome) VALUES('Fereydoun');
-INSERT INTO nomes(nome) VALUES('Giv');
-INSERT INTO nomes(nome) VALUES('Gordafarid');
-INSERT INTO nomes(nome) VALUES('Homayoun');
-INSERT INTO nomes(nome) VALUES('Hormoz');
-INSERT INTO nomes(nome) VALUES('Houshang');
-INSERT INTO nomes(nome) VALUES('Jamsheed');
-INSERT INTO nomes(nome) VALUES('Jamshid');
-INSERT INTO nomes(nome) VALUES('Kaveh');
-INSERT INTO nomes(nome) VALUES('Kayvan');
-INSERT INTO nomes(nome) VALUES('Khosrow');
-INSERT INTO nomes(nome) VALUES('Lohrasp');
-INSERT INTO nomes(nome) VALUES('Maziar');
-INSERT INTO nomes(nome) VALUES('Mehrdad');
-INSERT INTO nomes(nome) VALUES('Mithra');
-INSERT INTO nomes(nome) VALUES('Nader');
-INSERT INTO nomes(nome) VALUES('Nasrin');
-INSERT INTO nomes(nome) VALUES('Navid');
-INSERT INTO nomes(nome) VALUES('Nezam');
-INSERT INTO nomes(nome) VALUES('Nushin');
-INSERT INTO nomes(nome) VALUES('Parviz');
-INSERT INTO nomes(nome) VALUES('Ramin');
-INSERT INTO nomes(nome) VALUES('Rashid');
-INSERT INTO nomes(nome) VALUES('Rostam');
-INSERT INTO nomes(nome) VALUES('Sam');
-INSERT INTO nomes(nome) VALUES('Shahram');
-INSERT INTO nomes(nome) VALUES('Shapur');
-INSERT INTO nomes(nome) VALUES('Soroush');
-INSERT INTO nomes(nome) VALUES('Sohrab');
-INSERT INTO nomes(nome) VALUES('Tahmuras');
-INSERT INTO nomes(nome) VALUES('Vahid');
-INSERT INTO nomes(nome) VALUES('Vash');
-INSERT INTO nomes(nome) VALUES('Yazdegerd');
-INSERT INTO nomes(nome) VALUES('Zahhak');
-INSERT INTO nomes(nome) VALUES('Zal');
-INSERT INTO nomes(nome) VALUES('Zartosht');
-INSERT INTO nomes(nome) VALUES('Zia');
-INSERT INTO nomes(nome) VALUES('Zohreh');
-
+CREATE TABLE "nomes_femininos" (
+	"id"	INTEGER NOT NULL,
+	"nome"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
 
 CREATE TABLE "sobrenomes" (
 	"id"	INTEGER NOT NULL,
@@ -219,101 +93,9 @@ CREATE TABLE "sobrenomes" (
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
-INSERT INTO sobrenomes(sobrenome) VALUES('Lykaios');
-INSERT INTO sobrenomes(sobrenome) VALUES('Andreas');
-INSERT INTO sobrenomes(sobrenome) VALUES('Antonopoulos');
-INSERT INTO sobrenomes(sobrenome) VALUES('Bakirtzis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Christodoulou');
-INSERT INTO sobrenomes(sobrenome) VALUES('Demetriou');
-INSERT INTO sobrenomes(sobrenome) VALUES('Efstathiou');
-INSERT INTO sobrenomes(sobrenome) VALUES('Fotiadis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Georgiou');
-INSERT INTO sobrenomes(sobrenome) VALUES('Hatzis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Iordanou');
-INSERT INTO sobrenomes(sobrenome) VALUES('Katsaros');
-INSERT INTO sobrenomes(sobrenome) VALUES('Laskaris');
-INSERT INTO sobrenomes(sobrenome) VALUES('Mavros');
-INSERT INTO sobrenomes(sobrenome) VALUES('Nikolaidis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Oikonomou');
-INSERT INTO sobrenomes(sobrenome) VALUES('Papadopoulos');
-INSERT INTO sobrenomes(sobrenome) VALUES('Raptis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Samaras');
-INSERT INTO sobrenomes(sobrenome) VALUES('Tzimas');
-INSERT INTO sobrenomes(sobrenome) VALUES('Vasiliou');
-INSERT INTO sobrenomes(sobrenome) VALUES('Zografos');
-INSERT INTO sobrenomes(sobrenome) VALUES('Argyris');
-INSERT INTO sobrenomes(sobrenome) VALUES('Bakogiannis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Christakis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Doukas');
-INSERT INTO sobrenomes(sobrenome) VALUES('Eliopoulos');
-INSERT INTO sobrenomes(sobrenome) VALUES('Fountas');
-INSERT INTO sobrenomes(sobrenome) VALUES('Galanis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Hatzidakis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Ioannidis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Katsoulis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Livanios');
-INSERT INTO sobrenomes(sobrenome) VALUES('Michalakis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Nikitas');
-INSERT INTO sobrenomes(sobrenome) VALUES('Papageorgiou');
-INSERT INTO sobrenomes(sobrenome) VALUES('Rousakis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Sotiropoulos');
-INSERT INTO sobrenomes(sobrenome) VALUES('Tzakis');
-INSERT INTO sobrenomes(sobrenome) VALUES('Vlastos');
-INSERT INTO sobrenomes(sobrenome) VALUES('Zervos');
-INSERT INTO sobrenomes(sobrenome) VALUES('Aghassian');
-INSERT INTO sobrenomes(sobrenome) VALUES('Ardalan');
-INSERT INTO sobrenomes(sobrenome) VALUES('Bahmani');
-INSERT INTO sobrenomes(sobrenome) VALUES('Bakhtiar');
-INSERT INTO sobrenomes(sobrenome) VALUES('Chamani');
-INSERT INTO sobrenomes(sobrenome) VALUES('Darvish');
-INSERT INTO sobrenomes(sobrenome) VALUES('Ehsani');
-INSERT INTO sobrenomes(sobrenome) VALUES('Farzan');
-INSERT INTO sobrenomes(sobrenome) VALUES('Ghaffari');
-INSERT INTO sobrenomes(sobrenome) VALUES('Hedayat');
-INSERT INTO sobrenomes(sobrenome) VALUES('Jahanshah');
-INSERT INTO sobrenomes(sobrenome) VALUES('Karimi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Kaviani');
-INSERT INTO sobrenomes(sobrenome) VALUES('Mehrabi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Nafisi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Parvizi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Qorbani');
-INSERT INTO sobrenomes(sobrenome) VALUES('Rostami');
-INSERT INTO sobrenomes(sobrenome) VALUES('Shahbazi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Tabrizi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Vaziri');
-INSERT INTO sobrenomes(sobrenome) VALUES('Yazdani');
-INSERT INTO sobrenomes(sobrenome) VALUES('Zamani');
-INSERT INTO sobrenomes(sobrenome) VALUES('Amir');
-INSERT INTO sobrenomes(sobrenome) VALUES('Behrouz');
-INSERT INTO sobrenomes(sobrenome) VALUES('Chaman');
-INSERT INTO sobrenomes(sobrenome) VALUES('Dehghan');
-INSERT INTO sobrenomes(sobrenome) VALUES('Esmaili');
-INSERT INTO sobrenomes(sobrenome) VALUES('Fard');
-INSERT INTO sobrenomes(sobrenome) VALUES('Ganjavi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Hosseini');
-INSERT INTO sobrenomes(sobrenome) VALUES('Jafari');
-INSERT INTO sobrenomes(sobrenome) VALUES('Khodadadi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Latifi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Mahdavi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Nassiri');
-INSERT INTO sobrenomes(sobrenome) VALUES('Pahlavi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Rahimi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Sadri');
-INSERT INTO sobrenomes(sobrenome) VALUES('Tajik');
-INSERT INTO sobrenomes(sobrenome) VALUES('Zadeh');
-INSERT INTO sobrenomes(sobrenome) VALUES('Ahmadi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Ashrafi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Bakhtiari');
-INSERT INTO sobrenomes(sobrenome) VALUES('Cheraghchi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Djahani');
-INSERT INTO sobrenomes(sobrenome) VALUES('Ebrahimi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Fahimi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Golshani');
-INSERT INTO sobrenomes(sobrenome) VALUES('Hassani');
-INSERT INTO sobrenomes(sobrenome) VALUES('Isfahani');
-INSERT INTO sobrenomes(sobrenome) VALUES('Jalali');
-INSERT INTO sobrenomes(sobrenome) VALUES('Kazemi');
-INSERT INTO sobrenomes(sobrenome) VALUES('Mokhtari');
-INSERT INTO sobrenomes(sobrenome) VALUES('Nasiri');
-INSERT INTO sobrenomes(sobrenome) VALUES('Pirooz');
-INSERT INTO sobrenomes(sobrenome) VALUES('Rostamian');
+
+CREATE TABLE "valores_5_a_10"(
+	"id"	INTEGER NOT NULL,
+	"valor"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+)
--- DB/SQLs/triggers.sql
+++ DB/SQLs/triggers.sql
@@ -1,50 +1,38 @@
-CREATE TRIGGER IF NOT EXISTS cria_recrutas_apos_cadastro
-AFTER INSERT ON usuarios
+CREATE TRIGGER cria_recrutas_apos_cadastro
+AFTER INSERT ON jogadores
 FOR EACH ROW
 BEGIN
-    INSERT INTO recrutas (id_jogador, nome, ataque, defesa, inteligencia, forca, agilidade, idade, imagem_recruta_id, status_recruta)
-    VALUES (NEW.id,
-	(SELECT nome FROM nomes ORDER BY RANDOM() LIMIT 1) || ' ' || 
-    (SELECT sobrenome FROM sobrenomes ORDER BY RANDOM() LIMIT 1) AS nome, 
-	ABS(RANDOM() % 11) + 5, ABS(RANDOM() % 11) + 5, 1, 1,ELECT id FROM caminho_da_imagem_recrutas ORDER BY RANDOM() LIMIT 1
-	, 0); 
-	
-	INSERT INTO recrutas (id_jogador, nome, ataque, defesa, inteligencia, forca, agilidade, idade, imagem_recruta_id, status_recruta)
-	VALUES (NEW.id,
-	(SELECT nome FROM nomes ORDER BY RANDOM() LIMIT 1) || ' ' || 
-    (SELECT sobrenome FROM sobrenomes ORDER BY RANDOM() LIMIT 1) AS nome, 
-	ABS(RANDOM() % 11) + 5, ABS(RANDOM() % 11) + 5, 1, 1, 1, 16, 
-	SELECT id FROM imagem_recrutas ORDER BY RANDOM() LIMIT 1
-	, 0); 
-	
-	INSERT INTO recrutas (id_jogador, nome, ataque, defesa, inteligencia, forca, agilidade, idade, imagem_recruta_id, status_recruta)
-	VALUES (NEW.id,
-	(SELECT nome FROM nomes ORDER BY RANDOM() LIMIT 1) || ' ' || 
-    (SELECT sobrenome FROM sobrenomes ORDER BY RANDOM() LIMIT 1) AS nome, 
-	ABS(RANDOM() % 11) + 5, ABS(RANDOM() % 11) + 5, 1, 1, 1, 16, 
-	SELECT id FROM imagem_recrutas ORDER BY RANDOM() LIMIT 1
-	, 0);  
-END;
+	INSERT INTO recrutas (id_jogador, nome, ataque, defesa, inteligencia, forca, agilidade, idade, status_recruta, sexo)
+	VALUES(NEW.id, 'Sem nome', (SELECT valor FROM valores_5_a_10 ORDER BY RANDOM() LIMIT 1), (SELECT valor FROM valores_5_a_10 ORDER BY RANDOM() LIMIT 1), 1, 1, 1, 16, 0, CASE WHEN (RANDOM() % 2) = 0 THEN 'M' ELSE 'F' END);
 
-CREATE TRIGGER IF NOT EXISTS transforma_recruta_em_guerreiro
-AFTER UPDATE OF status_recruta ON recrutas
-WHEN NEW.status_recruta = 1
-BEGIN
-	INSERT INTO guerreiros(id_jogador, nome, ataque, defesa, inteligencia, forca, agilidade, idade, imagem_guerreiro_id, vitorias, nivel)
-	VALUES(new.id_jogador, NEW.nome, NEW.ataque, NEW.defesa, NEW.inteligencia, NEW.forca, NEW.agilidade, NEW.idade, NEW.imagem_recruta_id, 0, 1)
-END;
+	INSERT INTO recrutas (id_jogador, nome, ataque, defesa, inteligencia, forca, agilidade, idade, status_recruta, sexo)
+	VALUES(NEW.id, 'Sem nome', (SELECT valor FROM valores_5_a_10 ORDER BY RANDOM() LIMIT 1), (SELECT valor FROM valores_5_a_10 ORDER BY RANDOM() LIMIT 1), 1, 1, 1, 16, 0, CASE WHEN (RANDOM() % 2) = 0 THEN 'M' ELSE 'F' END);
 
-CREATE TRIGGER gera_recruta
-AFTER UPDATE OF dias_jogados ON jogadores
-FOR EACH ROW
-WHEN NEW.dias_jogados % 10 = 0
-BEGIN
-    INSERT INTO recrutas (id_jogador, nome, ataque, defesa, inteligencia, forca, agilidade, idade, imagem_recruta_id, status_recruta)
-    VALUES (NEW.id,
-	(SELECT nome FROM nomes ORDER BY RANDOM() LIMIT 1) || ' ' || (SELECT sobrenome FROM sobrenomes ORDER BY RANDOM() LIMIT 1) AS nome,
-	ABS(RANDOM() % 11) + 5 AS ataque,
-	ABS(RANDOM() % 11) + 5 AS defesa,
-	1, 1, 1, 16,
-	SELECT id FROM imagem_recrutas ORDER BY RANDOM() LIMIT 1
-	0);
-END;
+	INSERT INTO recrutas (id_jogador, nome, ataque, defesa, inteligencia, forca, agilidade, idade, status_recruta, sexo)
+	VALUES(NEW.id, 'Sem nome', (SELECT valor FROM valores_5_a_10 ORDER BY RANDOM() LIMIT 1), (SELECT valor FROM valores_5_a_10 ORDER BY RANDOM() LIMIT 1), 1, 1, 1, 16, 0, CASE WHEN (RANDOM() % 2) = 0 THEN 'M' ELSE 'F' END);
+END
+
+
+
+-- CREATE TRIGGER IF NOT EXISTS transforma_recruta_em_guerreiro
+-- AFTER UPDATE OF status_recruta ON recrutas
+-- WHEN NEW.status_recruta = 1
+-- BEGIN
+-- 	INSERT INTO guerreiros(id_jogador, nome, ataque, defesa, inteligencia, forca, agilidade, idade, imagem_guerreiro_id, vitorias, nivel)
+-- 	VALUES(new.id_jogador, NEW.nome, NEW.ataque, NEW.defesa, NEW.inteligencia, NEW.forca, NEW.agilidade, NEW.idade, NEW.imagem_recruta_id, 0, 1)
+-- END;
+
+-- CREATE TRIGGER gera_recruta
+-- AFTER UPDATE OF dias_jogados ON jogadores
+-- FOR EACH ROW
+-- WHEN NEW.dias_jogados % 10 = 0
+-- BEGIN
+--     INSERT INTO recrutas (id_jogador, nome, ataque, defesa, inteligencia, forca, agilidade, idade, imagem_recruta_id, status_recruta)
+--     VALUES (NEW.id,
+-- 	(SELECT nome FROM nomes ORDER BY RANDOM() LIMIT 1) || ' ' || (SELECT sobrenome FROM sobrenomes ORDER BY RANDOM() LIMIT 1) AS nome,
+-- 	(SELECT valor FROM valores_5_a_10 ORDER BY RANDOM() LIMIT 1) AS ataque,
+-- 	(SELECT valor FROM valores_5_a_10 ORDER BY RANDOM() LIMIT 1) AS defesa,
+-- 	1, 1, 1, 16,
+-- 	SELECT id FROM imagem_recrutas ORDER BY RANDOM() LIMIT 1
+-- 	0);
+-- END;
