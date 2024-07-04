b16a50fb5ece2b0648dc27661158c43259fdabda
Your Name <josuess13@gmail.com>
2024-07-04 08:36:13 -0300

    alterando imagens e criando trigger para evitar nomes duplicados

--- DB/SQLs/triggers.sql
+++ DB/SQLs/triggers.sql
@@ -61,11 +61,31 @@ BEGIN
 END;
 
 
-CREATE TRIGGER gera_recruta
+CREATE TRIGGER IF NOT EXISTS gera_recruta
 AFTER UPDATE OF dias_jogados ON jogadores
 FOR EACH ROW
 WHEN NEW.dias_jogados % 10 = 0 AND NEW.dias_jogados > 0
 BEGIN
     INSERT INTO recrutas (id_jogador, nome, ataque, defesa, inteligencia, forca, agilidade, idade, status_recruta, sexo)
 	VALUES(NEW.id, 'Sem nome', (SELECT valor FROM valores_5_a_10 ORDER BY RANDOM() LIMIT 1), (SELECT valor FROM valores_5_a_10 ORDER BY RANDOM() LIMIT 1), 1, 1, 1, 16, 0, CASE WHEN (RANDOM() % 2) = 0 THEN 'M' ELSE 'F' END);
+END;
+
+
+CREATE TRIGGER IF NOT EXISTS corrige_nomes_recruta_duplicados
+AFTER UPDATE OF nome ON recrutas
+FOR EACH ROW
+BEGIN
+    -- Verifica e corrige nomes duplicados femininos
+    UPDATE recrutas
+    SET nome = (
+        (SELECT nome FROM nomes_femininos ORDER BY RANDOM() LIMIT 1) || ' ' || (SELECT sobrenome FROM sobrenomes ORDER BY RANDOM() LIMIT 1)
+    )
+    WHERE nome = NEW.nome AND id != NEW.id AND NEW.sexo = 'F';
+
+    -- Verifica e corrige nomes duplicados masculinos
+    UPDATE recrutas
+    SET nome = (
+        (SELECT nome FROM nomes_masculinos ORDER BY RANDOM() LIMIT 1) || ' ' || (SELECT sobrenome FROM sobrenomes ORDER BY RANDOM() LIMIT 1)
+    )
+    WHERE nome = NEW.nome AND id != NEW.id AND NEW.sexo = 'M';
 END;
